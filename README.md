073f8212a2d559b825fb71678a3e59f62cc98385
mckayc <mckayjunk@gmail.com>
2015-10-17 13:19:09 -0600

    Add new icons and updated Readme.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# mouse_thing
-A Google Chrome extension
+# McLick - Mouse Cursor and Click Highlighter for screencast recordings.
+
+This is an offline Google Chrome extention. Use this extension for the following:
+* While doing a screencast recording, this will clearly show where your mouse cursor is and where you click on the page.
+* While doing a presentation, this can help so that your audience can clearly see where your mouse cursor is.
+* If you use a Chromecast, this can highlight your mouse cursor to make it easier to spot.
--- background.js
+++ background.js
@@ -3,11 +3,11 @@ var toggle = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   toggle = !toggle;
   if(toggle){
-    chrome.browserAction.setIcon({path: "icon-on.png", tabId:tab.id});
+    chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
     chrome.tabs.executeScript(tab.id, {code:"ext_on()"});
   }
   else{
-    chrome.browserAction.setIcon({path: "icon-off.png", tabId:tab.id});
+    chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
     chrome.tabs.executeScript(tab.id, {code:"ext_off()"});
   }
 });
